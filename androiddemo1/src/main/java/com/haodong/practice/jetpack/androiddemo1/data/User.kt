2cc46d2625ee358348a02b76993f296a3e714b83
haodongLing <linghaoDo@Gmail.com>
2020-06-23 11:38:26 +0800

    first commit

---
+++ androiddemo1/src/main/java/com/haodong/practice/jetpack/androiddemo1/data/User.kt
@@ -0,0 +1,10 @@
+package com.haodong.practice.jetpack.androiddemo1.data
+
+/**
+ * created by linghaoDo on 2020/6/23
+ * description:
+ *
+ * version:
+ */
+data class User(var name: String, var password: String) {
+}
---
+++ app/src/main/java/com/haodong/kotlintest/annotation/RouteType.java
@@ -0,0 +1,13 @@
+package com.haodong.kotlintest.annotation;
+
+import androidx.annotation.IntDef;
+
+/**
+ * created by linghaoDo on 2020/6/12
+ * description:
+ * <p>
+ * version:
+ */
+@IntDef({RouteTypeInt.WALK, RouteTypeInt.BUS, RouteTypeInt.CAR})
+@interface RouteType {
+}
