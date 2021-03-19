772c479cc2414b17d67518f7b7aae6185371148b
haodongLing <linghaoDo@Gmail.com>
2021-03-19 15:18:01 +0800

    add all

---
+++ wanandroid/src/main/java/com/haodong/practice/wanandroid/ui/NavigationActivity.kt
@@ -0,0 +1,23 @@
+package com.haodong.practice.wanandroid.ui
+
+import com.haodong.practice.mvvm.core.base.BaseActivity
+
+/**
+ * created by linghaoDo on 3/17/21
+ * description:
+ *
+ * version:
+ */
+class NavigationActivity :BaseActivity() {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLayoutId(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ wanandroid/src/main/java/com/haodong/practice/wanandroid/ui/TabFragment.kt
@@ -0,0 +1,24 @@
+package com.haodong.practice.wanandroid.ui
+
+import com.haodong.practice.mvvm.core.base.BaseFragment
+import com.haodong.practice.wanandroid.R
+
+/**
+ * created by linghaoDo on 3/17/21
+ * description:
+ *
+ * version:
+ */
+class TabFragment :BaseFragment() {
+    override fun getLayoutResId(): Int {
+        return  R.layout.activity_bottom_navigation
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+}
